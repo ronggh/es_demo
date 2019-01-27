1c9de3580d39cc91256baddafc01ed6037187680
ronggh@sina.cn <ronggh@sina.cn>
2019-01-27 22:19:35 +0800

    second commit

--- src/main/java/cn/alan/es/EsDemo.java
+++ src/main/java/cn/alan/es/EsDemo.java
@@ -4,6 +4,7 @@ import org.elasticsearch.client.transport.TransportClient;
 import org.elasticsearch.common.settings.Settings;
 import org.elasticsearch.common.transport.InetSocketTransportAddress;
 import org.elasticsearch.transport.client.PreBuiltTransportClient;
+import org.junit.Before;
 import org.junit.Test;
 
 
@@ -15,7 +16,7 @@ public class EsDemo {
 
 
     @SuppressWarnings("unchecked")
-    @Test
+    @Before
     public  void getClient() throws Exception {
         // 1 设置连接的集群名称
         Settings settings = Settings.builder().put("cluster.name", "es").build();
@@ -26,6 +27,23 @@ public class EsDemo {
 
         // 3 打印集群名称
         System.out.println(client.toString());
-
     }
+
+    @Test
+    public void createIndex(){
+        // 1 创建索引
+        client.admin().indices().prepareCreate("blog2").get();
+
+        // 2 关闭连接
+        client.close();
+    }
+    @Test
+    public void deleteIndex(){
+        // 1 删除索引
+        client.admin().indices().prepareDelete("blog2").get();
+
+        // 2 关闭连接
+        client.close();
+    }
+
 }
